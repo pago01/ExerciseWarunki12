7839fbb3a6f5b195dca41eed858f34b6773e47bf
ACTFYZZ <ACTFYZZ@PL10006677>
2023-08-21 12:55:45 +0200

    Add project files.

---
+++ ExerciseWarunki12/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace ExerciseWarunki12
+{
+    //Data structure declaration
+
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int ocena = 0;
+            string opis = "";
+            bool parseresult;
+            //Score read
+            do
+            {
+                Console.WriteLine("Podaj dzień tygodnia od 1 do 7");
+                parseresult = Int32.TryParse(Console.ReadLine(), out ocena);
+            }
+            while (!parseresult || ocena < 1 || ocena > 7);
+
+
+            //Score description
+            switch (ocena)
+            {
+                case 1:
+                    opis = "poniedziałek";
+                    break;
+                case 2:
+                    opis = "wtorek";
+                    break;
+                case 3:
+                    opis = "środa";
+                    break;
+                case 4:
+                    opis = "czwartek";
+                    break;
+                case 5:
+                    opis = "piątek";
+                    break;
+                case 6:
+                    opis = "sobota";
+                    break;
+                case 7:
+                    opis = "sobota";
+                    break;
+                default:
+                    opis = "niepoprawne dane";
+                    break;
+            }
+            Console.WriteLine(opis);
+        }
+    }
+}
